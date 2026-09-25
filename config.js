f3bb4e2d0852e6bea90d6ab7cfa02a440618367f
saritabedge <saritabedge@gmail.com>
2026-09-25 11:42:39 -0600

    Update locked token config

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ const ADMIN_CONFIG = {
   owner: "saritabedge",
   repo: "saritabedgeart",
   branch: "main",
-  lockedToken: null,
+  lockedToken: {"salt":"XFKEE6vrQqa6i8/3rsZL4Q==","iv":"3eubzh7ALJUkWHtv","data":"Os3uFsITeNGmdau9jwaAd0g2Vvu4ZvvRgC1/5aJ8gpt+Z7HXfISvvkorUZo8fiK2InEiS44fX+/jQyS1SKkhYMg17w7kUj1S7nIBZbPTuzHmVDJpVSpm8NAIJMdQIVkxU74pg+Vmvq6BJS8ALQ=="},
 };
