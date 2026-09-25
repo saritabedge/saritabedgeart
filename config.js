46c08ae9d118a50c1062d9b9db6353308a7a647c
saritabedge <saritabedge@gmail.com>
2026-09-25 11:59:55 -0600

    Update locked token

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ const ADMIN_CONFIG = {
   owner: "saritabedge",
   repo: "saritabedgeart",
   branch: "main",
-  lockedToken: {"salt":"9Uq7qPhFaeVJBxOyefyI5A==","iv":"Aw5sPxWHcgJNQ76G","data":"8eyX5KOyGVYRqewKwaCBQAwolBCOUvc7FSkbWg5YiPoEnDGnctlv7ojezBYyXK0ieHXJOchEsMt3ZdriZoCZVKPuLiPZ6CWOdhbzIeeN6QNrFWqL5gEWuC6eFcFW2dKs7uyYt+o2yw9A+s1H0g=="},
+  lockedToken: {"salt":"Qg2Aekp2Zwr65gL08z22+w==","iv":"jwvaWcKWJmvbbRpq","data":"flaNLU2M0o91DtxOglAHZnXhmuC/smI9mBqWudU0l7WxjHjvIxcHT8xSZ2qTJwO3qPDVIPkE1MuaZF9KK/pSnYmc3uBOqzLbNSk89sgDoodD+3ZehAgms/WUaIgu3yhslP7FZZsWjdfQcPo88g=="},
 };
